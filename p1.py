7606902412a27731cd32ba42ebb82e3fd885b7a8
chandu patil <chandu.patil005@gmail.com>
2019-02-04 17:20:30 +0530

    Chandru commited the program in repo

---
+++ p1.py
@@ -0,0 +1,8 @@
+print("sum is :",(10+20))
+
+a=10
+b=20
+print("Sum is : ",(a+b))
+
+sum=a+b
+print("Sum is : ",sum)
---
+++ p2.py
@@ -0,0 +1,19 @@
+a=10
+b=20
+print("Before swapping a is :",a," b is ",b)
+c=a
+a=b
+b=c
+print("After  swapping a is :",a," b is ",b)
+print("****************************************")
+print("Before swapping a is :",a," b is ",b)
+a,b=b,a
+print("After swapping a is :",a," b is ",b)
+print("****************************************")
+print("Before swapping a is :",a," b is ",b)
+a=a+b
+b=a-b
+a=a-b
+print("After swapping a is :",a," b is ",b)
+
+
